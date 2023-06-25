e217f923bbc79c3b895d7bca11e82749bd782a18
sirkotal <lordkotal2002@gmail.com>
2023-06-25 13:05:46 +0100

    Added some more keywords

--- kapla.h
+++ kapla.h
@@ -4,5 +4,12 @@
 #define mIr main
 #define togh cout
 #define Segh cin
+#define chugh if
+#define tlhogh else
+#define tlham struct
+#define qeng int
+#define raS double
+#define teH true
+#define ngeb false
 
 #endif
